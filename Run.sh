2fb444a17da5b497281aae7107b81402a89418ac
RecursiveDescent <lightspeed2@tutanota.com>
2024-09-01 08:04:46 -0400

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,9 +1,9 @@
 if [ "`md5sum IodineJS/*`" != "`cat sums`" ]; then
 	cd IodineJS
 
-	clang++-7 -std=c++17 -g -c api.cpp -o api.so
+	clang++ -std=c++17 -g -c api.cpp -o api.so
 
-	clang++-7 -std=c++17 -g main.cpp api.so -o ../main
+	clang++ -std=c++17 -g main.cpp api.so -o ../main
 
 	cd ..
 fi
